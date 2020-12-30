f0ea9ab57f9a50af4ba3a5a3628bafb3c526cdc5
Nadejde <andrei.nadejde@gmail.com>
2020-12-30 08:45:03 +0000

    added correct analytics id

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ReactGA from 'react-ga';
-ReactGA.initialize('G-6MY1RGF69C');
+
+ReactGA.initialize('UA-186271438-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
